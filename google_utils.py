bcd63f43c7f9ad6856540f67e16e10bb8cfb5a6a
sanjy <sanjy@feel-now.ru>
2021-05-30 11:57:34 +0500

    git info func

--- google_utils.py
+++ google_utils.py
@@ -4,6 +4,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
+
 def auth(scopes, credentials_filename, token_filename):
     """Auth in to google api and refresh token file
 
@@ -35,3 +36,35 @@ def auth(scopes, credentials_filename, token_filename):
     service = build('sheets', 'v4', credentials=creds)
 
     return service.spreadsheets()
+
+
+def get_sheets_info(spreadsheets, spreadsheets_id, sheet_number=0):
+    """Get sheet info 
+
+    Args:
+        spreadsheets (sting): Obj from auth finction.
+        spreadsheets_id (string): Spreedsheets id.
+        sheet_number (int, optional): Sheet number from spreedsheets.
+            As is displayed in google sheets in the browser starting from 0.
+            Defaults to 0.
+
+    Returns:
+        [dict]: Return dictionary with format:
+                'sheet_id': unic sheet id.
+                'title': sheet title.
+                'row_count': total row coun.
+                'conditional_formats': conditional formats.
+    """
+    spreadsheets = spreadsheets.get(spreadsheetId=spreadsheets_id).execute()
+
+    properties = spreadsheets['sheets'][sheet_number]['properties']
+    conditional_formats = spreadsheets['sheets'][sheet_number]['conditionalFormats']
+
+    sheet_info = {
+        'sheet_id': properties['sheetId'],
+        'title': properties['title'],
+        'row_count': properties['gridProperties']['rowCount'],
+        'conditional_formats': conditional_formats,
+    }
+
+    return sheet_info
--- main.py
+++ main.py
@@ -3,10 +3,15 @@ import google_utils
 scopes = "https://www.googleapis.com/auth/spreadsheets"
 credentials_filename = "credentials.json"
 token_filename = "token.json"
+spreedsheet_id = "1YTUhC0HaHSKriLKEeu6Byp_wOsVkJbxeOe1myCOEPjs"
+sheet_number = 0
+
 
 def main():
-    spredsheets = google_utils.auth(scopes, credentials_filename, token_filename)
-    print(spredsheets)
+    spredsheets = google_utils.auth(
+        scopes, credentials_filename, token_filename)
+    sheet_info = google_utils.get_sheets_info(spredsheets, spreedsheet_id)
+    print(sheet_info)
 
 
 if __name__ == "__main__":
